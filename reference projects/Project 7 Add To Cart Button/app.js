99af824acaac6af9993633e371a8634e7788d9ec
hemanthkumarmokara <100550244+hemanthkumarmokara@users.noreply.github.com>
2023-12-05 11:27:37 +0530

    Delete reference projects directory

--- reference projects/Project 7 Add To Cart Button/app.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".button").forEach((button) =>
-    button.addEventListener("click", (e) => {
-        if (!button.classList.contains("loading")) {
-            button.classList.add("loading");
-            setTimeout(() => button.classList.remove("loading"), 5700);
-        }
-        e.preventDefault();
-    })
-);
